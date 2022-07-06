19c40ea0d8304eb271e2a494a4d1f4baf7260943
mcrackk <107495800+mcrackk@users.noreply.github.com>
2022-07-06 11:27:53 -0400

    Update plotting_draft.py

--- plotting_draft.py
+++ plotting_draft.py
@@ -17,18 +17,19 @@ import astropy.io.ascii as at
 # Bonus 2: use the data directory and object name to search for files using glob.glob
 
 
-
 star_name = "IC2391b084426-5242_20190522"
-location =  "/Google Drive/Shared drives/DouglasGroup/data/Zorro_data_reformatted/"
-location_and_name = os.path.join(location, star_name)
-fname1=f"~{location_and_name}_562_raw.dat"
-fname2=f"~{location_and_name}_832_raw.dat"
+location =  "Google Drive/Shared drives/DouglasGroup/data/Zorro_data_reformatted/"
+#expanded_location = os.path.expanduser(location)
+location_and_name = os.path.join(os.path.expanduser(f"~/{location}"), star_name)
+fname1=f"{location_and_name}_562_raw.dat"
+fname2=f"{location_and_name}_832_raw.dat"
 edited_fname1 = os.path.expanduser(fname1)
 edited_fname2 = os.path.expanduser(fname2)
 filter1 = fname1.split('_')[-2]
 filter2 = fname2.split('_')[-2]
 dat1 = at.read(edited_fname1)
 dat2 = at.read(edited_fname2)
+
 
 plt.plot(dat1['ann_center'], dat1['delta_mag_limit'], 'k', label=f'{filter1} nm')
 plt.plot(dat2['ann_center'], dat2['delta_mag_limit'], 'r', label=f'{filter2} nm')
@@ -38,8 +39,7 @@ plt.legend()
 
 
 
-finder = glob.glob(os.path.join(r"~/Google Drive/Shared drives/DouglasGroup/data/Zorro_data_reformatted/IC2391b084426-5242_20190522_562_fit.dat"))
+finder = glob.glob(f'../{location}/{star_name}*raw.dat')
 print(finder)
+#finder now prints as a list of the raw.dat files for a particular star!
 
-# I initially tried to do an f string with the "location_and_name" and adding "*raw.dat" to specify I wanted all files ending in raw.dat using the syntax for
-#glob.glob but alas it didn't work
