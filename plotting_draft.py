f5abda6adb7650f061546766b8fa8191d376fa23
[mccrackk] <[mccrackk@lafayette.edu]>
2022-07-21 16:11:06 -0400

    got everything down to one step using a for loop

--- plotting_draft.py
+++ plotting_draft.py
@@ -5,6 +5,7 @@ Created on Mon Jul 18 11:12:26 2022
 
 @author: kiramccracken
 """
+
 import glob
 import os
 import numpy as np
@@ -17,26 +18,28 @@ def starplot(star_name):
     location =  "Google Drive/Shared drives/DouglasGroup/data/Zorro_data_reformatted/"
     location_and_name = os.path.join(os.path.expanduser(f"~/{location}"), star_name)
     finder = glob.glob(f'{location_and_name}*raw.dat')
-    fname1= finder[0]
-    fname2= finder[1]
-    edited_fname1 = os.path.expanduser(fname1)
-    edited_fname2 = os.path.expanduser(fname2)
-    filter1 = fname1.split('_')[-2]
-    filter2 = fname2.split('_')[-2]
-    dat1 = at.read(edited_fname1)
-    dat2 = at.read(edited_fname2)
-
-    #gathering all information necessary to plot data
-
-    plt.plot(dat1['ann_center'], dat1['delta_mag_limit'], 'k', label=f'{filter1} nm')
-    plt.plot(dat2['ann_center'], dat2['delta_mag_limit'], 'r', label=f'{filter2} nm')
+    print(finder)
+    for i, star_name in enumerate(finder):
+        if star_name.endswith('raw.dat'):
+            fname = finder[i]
+            #print(finder)
+            #print(finder[i])
+            edited_fname = os.path.expanduser(fname)
+            filter_ = fname.split('_')[-2]  
+            dat = at.read(edited_fname)
+    
+            #gathering all information necessary to plot data
+    
+            plt.plot(dat['ann_center'], dat['delta_mag_limit'], label=f'{filter_} nm')
     plt.xlabel('Separation [arcsec]')
     plt.ylabel('Delta Mag')
     plt.legend()
-    
-    
+  
+            
+            
+           
 
 
 if __name__== "__main__":
-    star_name = "IC2391b084426-5242_20190522"
+    star_name = "IC2391b084426-5242_"
     starplot(star_name)
