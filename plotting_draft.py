6412404b11e1be4a3f6c19eb09749a4694c141e2
[mccrackk] <[mccrackk@lafayette.edu]>
2022-07-26 14:16:19 -0400

    two functions now

--- plotting_draft.py
+++ plotting_draft.py
@@ -13,30 +13,34 @@ import matplotlib.pyplot as plt
 import astropy.io.ascii as at
 
 
-
-def starplot(star_name):
+ax = plt.subplot()
+def starplot(star_name, ax):
     location =  "Google Drive/Shared drives/DouglasGroup/data/Zorro_data_reformatted/"
     location_and_name = os.path.join(os.path.expanduser(f"~/{location}"), star_name)
     finder = glob.glob(f'{location_and_name}*raw.dat')
-    for i, star_name in enumerate(finder):
-        if star_name.endswith('raw.dat'):
+    color_dict = {'562': 'k', '832': 'r', '466': 'b', '716': 'g' } 
+    
+    for i, file_star_name in enumerate(finder):
+        if file_star_name.endswith('raw.dat'):
             fname = finder[i]
             edited_fname = os.path.expanduser(fname)
-            filter_ = fname.split('_')[-2]  
+            filter_num = fname.split('_')[-2]  
             dat = at.read(edited_fname)
     
             #gathering all information necessary to plot data
-            ax = plt.subplot()
-            ax.plot(dat['ann_center'], dat['delta_mag_limit'], label=f'{filter_} nm')
+            ax.plot(dat['ann_center'], dat['delta_mag_limit'], label=f'{filter_num} nm', color = color_dict[filter_num])     
+            ax.legend()
+  
+def one_star(star_name):
     ax.set_xlabel('Separation [arcsec]')
     ax.set_ylabel('Delta Mag')
-    ax.legend()
-  
-            
-            
+    ax.set_title(star_name)
+    #I moved the legend command into the loop because it wasn't working inside of one_star
+    starplot(star_name, ax)
            
 
 
 if __name__== "__main__":
-    star_name = "IC2391m084157-5252_"
-    starplot(star_name)
+    star_name = "IC2602b104256-6355"
+    one_star(star_name)
+    
