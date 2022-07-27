58af72142d71561cc5132f2c34551affcf7d4ab7
[mccrackk] <[mccrackk@lafayette.edu]>
2022-07-27 16:14:29 -0400

    "adding file"
     Please enter the commit message for your changes. Lines starting
    
    :wq

---
+++ list_stars.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 27 14:04:05 2022
+
+@author: kiramccracken
+"""
+
+#imports!!!
+#glob to get list of all of raw files
+#create empty list that will hold star names
+#loop over all files
+#split file name to extract star name only
+#append star name to list if not already there
+
+#write all names to new file
+
+import glob
+import os
+import numpy as np
+
+location =  "Google Drive/Shared drives/DouglasGroup/data/Zorro_data_reformatted/"
+location_expanded= os.path.join(os.path.expanduser(f"~/{location}"))
+find_all_stars = glob.glob(f'{location_expanded}*raw.dat')
+name_list = []
+for name in find_all_stars:
+    starname = name.split('/')[8].split('_')[-4]
+    name_list.append(starname)
+print(name_list)
+    
+    
