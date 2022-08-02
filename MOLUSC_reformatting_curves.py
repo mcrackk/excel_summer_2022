62c4fbc0b573215d202d0c882e97e6ab41211d35
[mccrackk] <[mccrackk@lafayette.edu]>
2022-08-02 11:11:39 -0400

    reformatting complete

--- MOLUSC_reformatting_curves.py
+++ MOLUSC_reformatting_curves.py
@@ -13,37 +13,60 @@ import os
 import glob
 
 
-# star_name = "IC2602b104422-6415" 
-
-# location =  "Google Drive/Shared drives/DouglasGroup/data/Zorro_data_reformatted/"
-# location_and_name = os.path.join(os.path.expanduser(f"~/{location}"), star_name)
-# finder = glob.glob(f'{location_and_name}*raw.dat') 
-# for i, file_star_name in enumerate(finder):
-#     if file_star_name.endswith('raw.dat'):
-#         fname = finder[i]
-#         edited_fname = os.path.expanduser(fname)
-#         dat = at.read(edited_fname)
-#         data_table = Table(data = [dat['ann_center'], dat['delta_mag_limit']], names=["Separation", "Delta Mag"])
-#         print(data_table)
-
-# format of files originally was "cluster_star_obsdate_filter" and the raw/fit.dat was added later
-#instead of worrying about the names of the filters, you can do this:
-    # filename.replace(".dat", "MOLUSC.dat)
-    # and it should have that name for every file
     
     
 star_name = "IC2602b104422-6415" 
 
-location =  "Google Drive/Shared drives/DouglasGroup/data/Zorro_data_reformatted/"
-location_and_name = os.path.join(os.path.expanduser(f"~/{location}"), star_name)
-finder = glob.glob(f'{location_and_name}*.dat') 
-print(finder)
-for i, file_star_name in enumerate(finder):
-    if file_star_name.endswith('.dat'):
-        fname = finder[i]
-        edited_fname = os.path.expanduser(fname)
-        dat = at.read(edited_fname)
-        data_table = Table(data = [dat['ann_center'], dat['delta_mag_limit']], names=["Separation", "Delta Mag"])
-        print(data_table)
-        MOLUSC_fname = edited_fname.replace(".dat", "MOLUSC.dat")
-        print(MOLUSC_fname)
+
+        
+def one_star_table(star_name):
+    location =  "Google Drive/Shared drives/DouglasGroup/data/Zorro_data_reformatted/"
+    location_and_name = os.path.join(os.path.expanduser(f"~/{location}"), star_name)
+    finder = glob.glob(f'{location_and_name}*.dat') 
+    for i, file_star_name in enumerate(finder):
+        if file_star_name.endswith('.dat'):
+            edited_fname = os.path.expanduser(file_star_name)
+            dat = at.read(edited_fname)
+            data_table = Table(data = [dat['ann_center'], dat['delta_mag_limit']], names=["Separation", "Delta Mag"])
+            print(data_table)
+            MOLUSC_fname = edited_fname.replace(".dat", "MOLUSC.dat")
+            new_pathname = MOLUSC_fname.replace('Zorro_data_reformatted', 'Zorro_to_MOLUSC')
+            at.write(data_table, new_pathname, overwrite = True)
+        
+
+star_names = at.read("all_zorro_targets.csv")
+
+ 
+
+  
+def every_star_table(star_names):
+    for star_name in star_names:
+        one_star_table(star_name)
+ 
+ 
+every_star_table(star_names['name'])
+
+
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
