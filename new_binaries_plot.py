f569738387e26b7fa3700ced6b6a518a131aacfc
[mccrackk] <[mccrackk@lafayette.edu]>
2022-10-08 17:20:16 -0400

    started new binary plot

---
+++ new_binaries_plot.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  4 21:15:16 2022
+
+@author: kiramccracken
+"""
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import astropy.io.ascii as at
+
+
+#host_name = "IC2391b084300-5354"
+
+ssfont = {'fontname':'sans-serif'}
+color_dict = {'purple 1': '#984ea3', 'red': '#e41a1c', 'grey': '#999999', 'pink': '#f781bf' }
+
+binary_location = "Google Drive/Shared drives/DouglasGroup/data/Zorro_speckle_info/Douglas_Binaries_GS_all.txt"
+binary_location_expanded = os.path.join(os.path.expanduser(f"~/{binary_location}"))
+    #on same plot with the curves 
+    #for confirmed_pair in binary_location_expanded:
+    #for name in binary_location_expanded:
+data = at.read(binary_location_expanded)
+#print(data)
+ax = plt.subplot()
+ax.plot(data['rho'], data['delm'], alpha = 0.5, color = color_dict['red'])
+ax.set_xlabel("Separation [arcsec]")
+ax.set_ylabel("Delta mag")
+ax.set_ylim(10, -0.5)
+ax.set_xlim(0, 1.2)
+ 
